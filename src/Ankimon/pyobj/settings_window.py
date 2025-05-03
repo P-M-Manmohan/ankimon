0d76545b4ad344e35d8896d9a88fada54d347192
h0tp-ftw <noddy.manan@gmail.com>
2025-05-03 08:20:53 -0600

    Revert "Merge branch 'main' into fix/settings-changed-dialog"
    This reverts commit b2957b651aabecb28d3d81da0e605c07f59b3335, reversing
    changes made to a7cde753ce02bf515bc094aceee0a1bbaadc300e.
    
    	modified:   README.md
    	modified:   src/Ankimon/__init__.py
    	modified:   src/Ankimon/lang/en_text.json
    	modified:   src/Ankimon/pyobj/collection_dialog.py
    	modified:   src/Ankimon/pyobj/settings_window.py

--- src/Ankimon/pyobj/settings_window.py
+++ src/Ankimon/pyobj/settings_window.py
@@ -158,7 +158,6 @@ class SettingsWindow(QMainWindow):
         excluded_patterns = {
         'mypokemon', 'mainpokemon', 'pokemon_collection', 'trainer.cash'
         }
-
         # Check for changes, excluding 'mypokemon' and 'mainpokemon'
         changed_settings = {
             key: self.config[key]
